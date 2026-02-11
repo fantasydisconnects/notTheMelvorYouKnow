34162243c1ee4f30265ac74085593b593778d55e
Johnny Van Slyke <207943250+fantasy-disconnects@users.noreply.github.com>
2026-02-10 22:29:19 -0500

    Add level cap increase, add new gems

--- setup.mjs
+++ setup.mjs
@@ -1,3 +1,11 @@
-export function setup(ctx) { 
-    
+export function setup(ctx) {
+  ctx.patch(Skill, 'maxLevelCap').get(function () { return 200; });
+  if (cloudManager.hasAoDEntitlementAndIsEnabled) {
+    ctx.patch(Archaeology, 'maxLevelCap').get(function () { return 200; });
+    ctx.patch(Cartography, 'maxLevelCap').get(function () { return 200; });
+  }
+  if (cloudManager.hasItAEntitlementAndIsEnabled) {
+    ctx.patch(Corruption, 'maxLevelCap').get(function () { return 200; });
+    ctx.patch(Harvesting, 'maxLevelCap').get(function () { return 200; });
+  }
 }
