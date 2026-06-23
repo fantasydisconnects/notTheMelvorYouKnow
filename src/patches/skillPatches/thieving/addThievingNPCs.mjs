a94cac6e837c2707b9e8a869d6d220ffe551a49f
Johnny Van Slyke <207943250+fantasy-disconnects@users.noreply.github.com>
2026-06-22 21:29:16 -0400

    Add Alchemist Thieving Target and Alembic Unique

--- src/patches/skillPatches/thieving/addThievingNPCs.mjs
+++ src/patches/skillPatches/thieving/addThievingNPCs.mjs
@@ -1,8 +1,10 @@
 export function addThievingNPCs() {
 
   const market = game.thieving.areas.getObjectByID("melvorF:FARMERS_MARKET");
+  const tower = game.thieving.areas.getObjectByID("melvorF:WIZARD_TOWER");
 
   const gardener = game.thieving.actions.getObjectByID("expandedAreas:PETUNIA_MASTER_GARDENER");
+  const alchemist = game.thieving.actions.getObjectByID("expandedAreas:ALCHEMIST");
 
   market.npcs.push(gardener);
 }
