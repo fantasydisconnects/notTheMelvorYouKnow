3ae16b02ca0baea93975f69fd9110cb2694ef5bb
Johnny Van Slyke <207943250+fantasy-disconnects@users.noreply.github.com>
2026-06-23 23:30:09 -0400

    Fix gamebreaking Magic Golbin Corruption bug

--- src/patches/skillPatches/thieving/addThievingNPCs.mjs
+++ src/patches/skillPatches/thieving/addThievingNPCs.mjs
@@ -1,10 +1,14 @@
 export function addThievingNPCs() {
 
   const market = game.thieving.areas.getObjectByID("melvorF:FARMERS_MARKET");
+  const port = game.thieving.areas.getObjectByID("melvorF:PORT_OF_LEMVOR");
   const tower = game.thieving.areas.getObjectByID("melvorF:WIZARD_TOWER");
 
   const gardener = game.thieving.actions.getObjectByID("expandedAreas:PETUNIA_MASTER_GARDENER");
+  const retired_pirate = game.thieving.actions.getObjectByID("expandedAreas:RETIRED_PIRATE");
   const alchemist = game.thieving.actions.getObjectByID("expandedAreas:ALCHEMIST");
 
   market.npcs.push(gardener);
+  port.npcs.push(retired_pirate);
+  tower.npcs.push(alchemist);
 }
