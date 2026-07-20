ef6614b2c4bb6db2aa21a5c345e718719a5c30da
Johnny Van Slyke <207943250+fantasy-disconnects@users.noreply.github.com>
2026-07-20 13:21:52 -0400

    add WTM support, remove weapon specs

--- assets/src/setup.mjs
+++
@@ -1,9 +0,0 @@
-export function setup(ctx) {
-
-  ctx.patch(Skill, 'maxLevelCap').get(function () { return 200; });
-
-  ctx.onCharacterLoaded(async (ctx) => {
-    mod.api.mythCombatSimulator?.registerNamespace('expandedAreas');
-  });
-
-}
